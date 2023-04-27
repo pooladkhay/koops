84842f5e43f1ee12734497c3143fd374e54fed5c
Mohammad Javad Pooladkhay <m.pooladkhay@gmail.com>
2023-04-27 11:58:55 +0100

    add ebpf map

--- koops-ebpf/src/main.rs
+++ koops-ebpf/src/main.rs
@@ -1,8 +1,15 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::{macros::kprobe, programs::ProbeContext};
+use aya_bpf::{
+    macros::{kprobe, map},
+    maps::Array,
+    programs::ProbeContext,
+};
 use aya_log_ebpf::info;
+
+#[map(name = "OOPS_COUNT")]
+pub static mut OOPS_COUNT: Array<u64> = Array::with_max_entries(1, 0);
 
 #[kprobe(name = "koops")]
 pub fn koops(ctx: ProbeContext) -> u32 {
@@ -14,6 +21,12 @@ pub fn koops(ctx: ProbeContext) -> u32 {
 
 fn try_koops(ctx: ProbeContext) -> Result<u32, u32> {
     info!(&ctx, "function oops_exit called");
+
+    match unsafe { OOPS_COUNT.get_ptr_mut(0) } {
+        Some(oops_count) => unsafe { *oops_count += 1 },
+        None => {}
+    }
+
     Ok(0)
 }
 
